4439a49dea7f49b2b0506fb09f59d1055e640065
“ibramosama” <BM@LAB.local>
2024-05-08 13:29:07 +0300

    update entites

--- src/main/java/com/ecommerce/Entites/Operationn.java
+++ src/main/java/com/ecommerce/Entites/Operationn.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Table(name = "operationns")
 public class Operationn {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
