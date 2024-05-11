bc46a8a63c298b72eb6f1aa2ff73630229abee90
ebi-java <ebi.lab22@gmail.com>
2024-05-11 12:14:57 +0300

    habiba and omneya subscription doa

--- src/main/java/com/ecommerce/customer/subscription/service/SubscriptionService.java
+++ src/main/java/com/ecommerce/customer/subscription/service/SubscriptionService.java
@@ -1,11 +1,16 @@
 package com.ecommerce.customer.subscription.service;
 
 import com.ecommerce.Entites.Account;
+import com.ecommerce.Entites.Product;
 import com.ecommerce.Entites.Subscription;
+import com.ecommerce.customer.product.service.CustomerProductService;
 import com.ecommerce.customer.subscription.dao.SubscriptionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +19,21 @@ public class SubscriptionService {
 
     @Autowired
     private SubscriptionDAO subscriptionDAO;
+    private CustomerProductService customerProductService;
 
     public void addSubscription(Subscription s){
        subscriptionDAO.save(s);
+    }
+    public void insertSubscription(Subscription s ,int id ){
+        LocalDate currentDate = LocalDate.now();
+        Optional<Product> CustomerProductService = customerProductService.findById(id);
+        if (CustomerProductService.isEmpty()) {
+            return;
+        }
+
+        s.setProduct(CustomerProductService.get());
+        s.setStartDate(currentDate);
+        subscriptionDAO.save(s);
     }
    public List<Subscription>findByAccount_AccountNumber(String account){
        return subscriptionDAO.findByAccount_AccountNumber(account);
