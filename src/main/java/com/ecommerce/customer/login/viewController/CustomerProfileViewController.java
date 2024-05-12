db9790280c26b8d775aba85275ccdfea1262e923
ebi-java <ebi.lab22@gmail.com>
2024-05-12 14:43:36 +0300

    Hossam & Sha3lan update Customer Profile view controller&JSP

--- src/main/java/com/ecommerce/customer/login/viewController/CustomerProfileViewController.java
+++ src/main/java/com/ecommerce/customer/login/viewController/CustomerProfileViewController.java
@@ -9,11 +9,15 @@ import com.ecommerce.admin.customer.service.CustomerService;
 import com.ecommerce.admin.login.service.CustomLoginService;
 import com.ecommerce.admin.product.service.ProductServiceImp;
 import com.ecommerce.customer.subscription.service.SubscriptionService;
+import com.ecommerce.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,8 +42,12 @@ public class CustomerProfileViewController {
 
     @GetMapping("/customer/profile")
     public String customerProfile(Model model ){
-        String username = (String)SecurityContextHolder.getContext().getAuthentication().getCredentials();
-        User customer = customLoginService.login("EBusiness");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails principal = (CustomUserDetails) auth.getPrincipal();
+
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User customer = customLoginService.login(userDetails.getUsername());
 
         List<Account> allCustomerAccounts = accountService.getAllCustomerAccounts(customer.getUserDetail().getId());
         List<List<Subscription>> subscriptions = new ArrayList<>();
@@ -54,11 +62,4 @@ public class CustomerProfileViewController {
         model.addAttribute("subscriptions",subscriptions);
         return "CustomerProfile";
     }
-
-//    @RequestMapping("/customer/update")
-//    public String updateProfile(@ModelAttribute Customer customer){
-//        Customer customer = customerService.getCustomerById(id);
-//        customerService.saveCustomer(customer);
-//        return "home-customer";
-//    }
 }
