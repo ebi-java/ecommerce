091e60cf0bd1df1ce0fc1f09941bfca297c386ce
Sherif <crash5band@gmail.com>
2024-05-12 11:43:17 +0300

    Resolve merge conflict - Sherif

--- src/main/java/com/ecommerce/customer/subscription/viewController/SubscriptionViewController.java
+++ src/main/java/com/ecommerce/customer/subscription/viewController/SubscriptionViewController.java
@@ -1,24 +1,14 @@
 package com.ecommerce.customer.subscription.viewController;
 
-<<<<<<<<< Temporary merge branch 1
-import com.ecommerce.Entites.Customer;
-import com.ecommerce.Entites.Product;
 import com.ecommerce.Entites.Subscription;
-import com.ecommerce.admin.customer.service.CustomerService;
+import com.ecommerce.Entites.User;
+import com.ecommerce.admin.login.service.CustomLoginService;
 import com.ecommerce.admin.product.service.ProductService;
-import com.ecommerce.customer.login.service.CustomerLoginService;
 import com.ecommerce.customer.product.service.CustomerProductService;
 import com.ecommerce.customer.subscription.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-=========
-import com.ecommerce.Entites.Subscription;
-import com.ecommerce.customer.product.service.CustomerProductService;
-import com.ecommerce.customer.subscription.service.SubscriptionService;
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>>>> Temporary merge branch 2
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,41 +19,29 @@ public class SubscriptionViewController {
 
     @Autowired
     private SubscriptionService subscriptionService;
-<<<<<<<<< Temporary merge branch 1
     @Autowired
-    private CustomerLoginService customerService;
+    private CustomLoginService loginService;
     @Autowired
     private ProductService productService;
+    @Autowired
+    private CustomerProductService customerProductService;
     @GetMapping("/{id}")
     public String show(Model model,@PathVariable("id") int id){
         if(productService.getProduct(id).getId() == id) {
             model.addAttribute("products", productService.getProduct(id));
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//            UserDetails customer = (UserDetails) authentication.getPrincipal();
-            Customer c = customerService.login(authentication.getName());
-            model.addAttribute("accounts", c.getAccounts());
+            User user = loginService.login(authentication.getName());
+//            model.addAttribute("accounts", );
         }else {
             model.addAttribute("message", "No product here");
         }
         return "subscription";
     }
 
-
-    @RequestMapping ("/insert/{id}")
-    public String insertSubscription(@ModelAttribute("subscription") Subscription s, Model model, @PathVariable("id") int id) {
-=========
-    private CustomerProductService customerProductService;
-
-
     @RequestMapping ("/insert/{id}")
     public String insertSubscription(@ModelAttribute("subscription") Subscription s, Model model,@PathVariable("id") int id) {
->>>>>>>>> Temporary merge branch 2
         subscriptionService.insertSubscription(s,id);
         model.addAttribute("subscription", new Subscription());
         return "subscription";
     }
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
 }
